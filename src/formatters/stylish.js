62e7e29b342580bea770e4308a083c2151b7e8fc
Skryam <skryams@gmail.com>
2024-04-05 17:46:51 +0500

    refac

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
 
 const objToString = (getObj, depth) => {
-  console.log(depth)
   const space = ' '.repeat(2 * depth);
   const getKeys = Object.keys(getObj);
   const bracketIndent = ' '.repeat((2 * depth) - 2);
@@ -9,59 +8,58 @@ const objToString = (getObj, depth) => {
     const value = getObj[key];
     if (!_.isObject(value)) {
       return `${space}  ${key}: ${value}`;
-    } else return `${space}  ${key}: ${objToString(value, depth + 2)}`;
-  })
+    } return `${space}  ${key}: ${objToString(value, depth + 2)}`;
+  });
   return [
     '{',
     ...result,
     `${bracketIndent}}`,
   ].join('\n');
- }
+};
 
 const stylishFormat = (diffObject) => {
-
+  console.log(diffObject.common);
   const iter = (obj, depth) => {
     const keys = Object.keys(obj);
     const indentSize = 2 * depth;
     const currentSpace = ' '.repeat(indentSize);
     const bracketIndent = ' '.repeat(indentSize - 2);
-     
+
     const lines = keys.flatMap((key) => {
-          if (obj[key].case === 'equal') {
-            return `${currentSpace}  ${key}: ${obj[key].value}`;
-          }
-           // Если же данные разные
-          else if (obj[key].case === 'updated') {
-            const value1 = obj[key].value[0];
-            const value2 = obj[key].value[1];
-            if (!_.isObject(value1) && !_.isObject(value2)) {
-            return [`${currentSpace}- ${key}: ${value1}`, `${currentSpace}+ ${key}: ${value2}`];
-            }
-            else if (_.isObject(value1) && !_.isObject(value2)) {
-              return [`${currentSpace}- ${key}: ${objToString(value1, depth + 2)}`, `${currentSpace}+ ${key}: ${value2}`]
-            }
-            else {
-             return [`${currentSpace}- ${key}: ${value1}`, `${currentSpace}+ ${key}: ${objToString(value2, depth + 2)}`]
-            }
-          }
-
-        else if (obj[key].case === 'nested') {
-          return `${currentSpace}  ${key}: ${iter(obj[key].value, depth + 2)}`;
-        }
-
-         // Уникальные из первого
-        else if (obj[key].case === 'deleted') {
-          if (!_.isObject(obj[key].value)) {
-          return `${currentSpace}- ${key}: ${obj[key].value}`;
-          } else return `${currentSpace}- ${key}: ${objToString(obj[key].value, depth + 2)}`;
-        }
-       // Уникальные из второго
-       else if (obj[key].case === 'added') {
-        if (!_.isObject(obj[key].value)) {
-        return `${currentSpace}+ ${key}: ${obj[key].value}`;
-        } else return `${currentSpace}+ ${key}: ${objToString(obj[key].value, depth + 2)}`;
+      if (obj[key].case === 'equal') {
+        return `${currentSpace}  ${key}: ${obj[key].value}`;
       }
-    })
+      // Если же данные разные
+      if (obj[key].case === 'updated') {
+        const value1 = obj[key].previousValue;
+        const value2 = obj[key].newValue;
+        if (!_.isObject(value1) && !_.isObject(value2)) {
+          return [`${currentSpace}- ${key}: ${value1}`, `${currentSpace}+ ${key}: ${value2}`];
+        }
+        if (_.isObject(value1) && !_.isObject(value2)) {
+          return [`${currentSpace}- ${key}: ${objToString(value1, depth + 2)}`, `${currentSpace}+ ${key}: ${value2}`];
+        }
+
+        return [`${currentSpace}- ${key}: ${value1}`, `${currentSpace}+ ${key}: ${objToString(value2, depth + 2)}`];
+      }
+
+      if (obj[key].case === 'nested') {
+        return `${currentSpace}  ${key}: ${iter(obj[key].value, depth + 2)}`;
+      }
+
+      // Уникальные из первого
+      if (obj[key].case === 'deleted') {
+        if (!_.isObject(obj[key].value)) {
+          return `${currentSpace}- ${key}: ${obj[key].value}`;
+        } return `${currentSpace}- ${key}: ${objToString(obj[key].value, depth + 2)}`;
+      }
+      // Уникальные из второго
+      if (obj[key].case === 'added') {
+        if (!_.isObject(obj[key].value)) {
+          return `${currentSpace}+ ${key}: ${obj[key].value}`;
+        } return `${currentSpace}+ ${key}: ${objToString(obj[key].value, depth + 2)}`;
+      }
+    });
 
     return [
       '{',
@@ -70,6 +68,6 @@ const stylishFormat = (diffObject) => {
     ].join('\n');
   };
   return iter(diffObject, 1);
-}
+};
 
 export default stylishFormat;
--- src/indexDiff.js
+++ src/indexDiff.js
@@ -7,33 +7,25 @@ const getDiffObject = (file1, file2) => {
 
   const iter = (currentValue1, currentValue2, depth) => {
     let result = {};
-    const objectsKeys = [Object.keys(currentValue1), Object.keys(currentValue2)].flat();
+    const objectsKeys = [...Object.keys(currentValue1), ...Object.keys(currentValue2)];
     const uniqueKeys = objectsKeys.filter((item, index) => objectsKeys.indexOf(item) === index).sort();
-    const lines = uniqueKeys.filter((key) => {
-      // Если есть в обоих
-      if (Object.hasOwn(currentValue1, key) && Object.hasOwn(currentValue2, key)) {
-        // Если оба НЕ объекты
-        if (!_.isObject(currentValue1[key]) && !_.isObject(currentValue2[key])) {
-          // Если равны по значениям
-            if (currentValue1[key] === currentValue2[key]) {
-               result[key] = { case: 'equal', value: currentValue1[key] };
-            } else {
-              result[key] = { case: 'updated', value: [currentValue1[key], currentValue2[key]] };
-           }
-          } // Если оба объекты
-          else if (_.isObject(currentValue1[key]) && _.isObject(currentValue2[key])) {
+    const lines = uniqueKeys.map((key) => {
+           // Если оба объекты
+          if (_.isObject(currentValue1[key]) && _.isObject(currentValue2[key])) {
              result[key] = { case: 'nested', value: iter(currentValue1[key], currentValue2[key], depth + 2) };
-          } // Если же данные разные
-          else {
-             result[key] = { case: 'updated', value: [currentValue1[key], currentValue2[key]] };
+          } // Уникальные из первого 
+          else if (Object.hasOwn(currentValue1, key) && !Object.hasOwn(currentValue2, key)) {
+               result[key] = { case: 'deleted', value: currentValue1[key] };
+            }
+           // Уникальные из второго
+          else if (!Object.hasOwn(currentValue1, key) && Object.hasOwn(currentValue2, key)) {
+               result[key] = { case: 'added', value: currentValue2[key] };
           }
-      } // Уникальные из первого 
-      else if (Object.hasOwn(currentValue1, key) && !Object.hasOwn(currentValue2, key)) {
-           result[key] = { case: 'deleted', value: currentValue1[key] };
-        }
-       // Уникальные из второго
-      else {
-           result[key] = { case: 'added', value: currentValue2[key] };
+           // Если же данные разные
+          else if (currentValue1[key] !== currentValue2[key]) {
+             result[key] = { case: 'updated', previousValue: currentValue1[key], newValue: currentValue2[key] };
+          } else {
+        result[key] = { case: 'equal', value: currentValue1[key] };
       }
     })
     return result;
