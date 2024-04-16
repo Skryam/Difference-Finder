83ab4fef88dbb577da4e31462e0c71181798e5e8
Skryam <skryams@gmail.com>
2024-04-16 09:56:06 +0500

    refac

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,40 +1,42 @@
 import path from 'node:path';
 import { cwd } from 'node:process';
 import { shouldBeStylish, shouldBePlain, shouldBeJSON } from '../__fixtures__/shouldBe.js';
-import buildDiff from '../src/buildDiff.js';
-import getFormat from '../src/formatters/index.js';
+import gendiff from '../src/index.js';
 
 const currentDirectory = cwd();
-const fileFromFixtures = (file) => path.join(currentDirectory, 'bin', '__fixtures__', file);
+const fileFromFixtures = (file) => path.join(currentDirectory, '__fixtures__', file);
+
+const jsonFilesFormat = (format) => gendiff(fileFromFixtures('deep1.json'), fileFromFixtures('deep2.json'), format);
+const yamlFilesFormat = (format) => gendiff(fileFromFixtures('deep1.yaml'), fileFromFixtures('deep2.yml'), format);
 
 describe('diff', () => {
   describe('stylish format', () => {
     it('should return diff in stylish format for json files', () => {
-      expect(stylishFormat(getDiffObject(fileFromFixtures('deep1.json'), fileFromFixtures('deep2.json')))).toEqual(shouldBeStylish);
+      expect(jsonFilesFormat('stylish')).toEqual(shouldBeStylish);
     });
 
     it('should return diff in stylish format for yaml files', () => {
-      expect(stylishFormat(getDiffObject(fileFromFixtures('deep1.yaml'), fileFromFixtures('deep2.yml')))).toEqual(shouldBeStylish);
+      expect(yamlFilesFormat('stylish')).toEqual(shouldBeStylish);
     });
   });
 
   describe('json format', () => {
     it('should return diff in json format for json files', () => {
-      expect(jsonFormat(getDiffObject(fileFromFixtures('deep1.json'), fileFromFixtures('deep2.json')))).toEqual(shouldBeJSON);
+      expect(jsonFilesFormat('json')).toEqual(shouldBeJSON);
     });
 
     it('should return diff in json format for yaml files', () => {
-      expect(jsonFormat(getDiffObject(fileFromFixtures('deep1.yaml'), fileFromFixtures('deep2.yml')))).toEqual(shouldBeJSON);
+      expect(yamlFilesFormat('json')).toEqual(shouldBeJSON);
     });
   });
 
   describe('plain format', () => {
     it('should return diff in plain format for json files', () => {
-      expect(plainFormat(getDiffObject(fileFromFixtures('deep1.json'), fileFromFixtures('deep2.json')))).toEqual(shouldBePlain);
+      expect(jsonFilesFormat('plain')).toEqual(shouldBePlain);
     });
 
     it('should return diff in plain format for yaml files', () => {
-      expect(plainFormat(getDiffObject(fileFromFixtures('deep1.yaml'), fileFromFixtures('deep2.yml')))).toEqual(shouldBePlain);
+      expect(yamlFilesFormat('plain')).toEqual(shouldBePlain);
     });
   });
 });
