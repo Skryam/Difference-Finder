937e70dd2fe8e1536e7ee3797820705e55f820f8
Skryam <skryams@gmail.com>
2024-04-16 15:26:20 +0500

    refac

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,33 +10,14 @@ const jsonFilesFormat = (format) => gendiff(fileFromFixtures('deep1.json'), file
 const yamlFilesFormat = (format) => gendiff(fileFromFixtures('deep1.yaml'), fileFromFixtures('deep2.yml'), format);
 
 describe('diff', () => {
-  describe('stylish format', () => {
-    it('should return diff in stylish format for json files', () => {
-      expect(jsonFilesFormat('stylish')).toEqual(shouldBeStylish);
-    });
+  const formats = [{ formatCase: 'stylish', shouldBe: shouldBeStylish }, { formatCase: 'plain', shouldBe: shouldBePlain },
+    { formatCase: 'json', shouldBe: shouldBeJSON }];
 
-    it('should return diff in stylish format for yaml files', () => {
-      expect(yamlFilesFormat('stylish')).toEqual(shouldBeStylish);
-    });
+  it.each(formats)('should return diff in %s format for json files', (format) => {
+    expect(jsonFilesFormat(format.formatCase)).toEqual(format.shouldBe);
   });
 
-  describe('json format', () => {
-    it('should return diff in json format for json files', () => {
-      expect(jsonFilesFormat('json')).toEqual(shouldBeJSON);
-    });
-
-    it('should return diff in json format for yaml files', () => {
-      expect(yamlFilesFormat('json')).toEqual(shouldBeJSON);
-    });
-  });
-
-  describe('plain format', () => {
-    it('should return diff in plain format for json files', () => {
-      expect(jsonFilesFormat('plain')).toEqual(shouldBePlain);
-    });
-
-    it('should return diff in plain format for yaml files', () => {
-      expect(yamlFilesFormat('plain')).toEqual(shouldBePlain);
-    });
+  it.each(formats)('should return diff in %s format for yaml files', (format) => {
+    expect(yamlFilesFormat(format.formatCase)).toEqual(format.shouldBe);
   });
 });
--- src/buildDiff.js
+++ src/buildDiff.js
@@ -12,53 +12,22 @@ const buildDiff = (filePath1, filePath2) => {
       .sort();
     return uniqueSortedKeys.reduce((acc, key) => {
       if (_.isObject(currentValue1[key]) && _.isObject(currentValue2[key])) {
-        return {
-          ...acc,
-          [key]: {
-            case: 'nested',
-            value: iter(currentValue1[key], currentValue2[key], depth + 2),
-          },
-        };
+        return { ...acc, [key]: { case: 'nested', value: iter(currentValue1[key], currentValue2[key], depth + 2) } };
       }
 
       if (Object.hasOwn(currentValue1, key) && !Object.hasOwn(currentValue2, key)) {
-        return {
-          ...acc,
-          [key]: {
-            case: 'deleted',
-            value: currentValue1[key],
-          },
-        };
+        return { ...acc, [key]: { case: 'deleted', value: currentValue1[key] } };
       }
 
       if (!Object.hasOwn(currentValue1, key) && Object.hasOwn(currentValue2, key)) {
-        return {
-          ...acc,
-          [key]: {
-            case: 'added',
-            value: currentValue2[key],
-          },
-        };
+        return { ...acc, [key]: { case: 'added', value: currentValue2[key] } };
       }
 
       if (currentValue1[key] !== currentValue2[key]) {
-        return {
-          ...acc,
-          [key]: {
-            case: 'updated',
-            previousValue: currentValue1[key],
-            newValue: currentValue2[key],
-          },
-        };
+        return { ...acc, [key]: { case: 'updated', previousValue: currentValue1[key], newValue: currentValue2[key] } };
       }
 
-      return {
-        ...acc,
-        [key]: {
-          case: 'equal',
-          value: currentValue1[key],
-        },
-      };
+      return { ...acc, [key]: { case: 'equal', value: currentValue1[key] } };
     }, {});
   };
   return iter(parsedFile1, parsedFile2, 1);
